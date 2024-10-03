61415e7b0a1031148a13c709b199a8e75aa191d4
Raheem Rajpoot <raheemrajpoot678@gmail.com>
2024-10-03 12:10:42 +0500

    update for backend

--- Backend/server.js
+++ Backend/server.js
@@ -13,17 +13,9 @@ import { app, server } from "./socket/socket.js";
 
 configDotenv();
 
-const __dirname = path.resolve();
-
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,8 +24,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messagesRouter);
 app.use("/api/users", userRouter);
-
-app.options("*", cors());
 
 app.use(globalErrorController);
 
