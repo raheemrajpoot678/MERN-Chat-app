5a75fba1bb595975c4fece12ce9e04d6bef29cf5
Raheem Rajpoot <raheemrajpoot678@gmail.com>
2024-10-03 09:18:13 +0500

    Deployment

---
+++ Backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+import path from "path";
+import "colors";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRouter from "./routes/auth.routes.js";
+import messagesRouter from "./routes/message.routes.js";
+import userRouter from "./routes/user.routes.js";
+import { DB } from "./database/Db.js";
+import { globalErrorController } from "./controllers/globle.error.controller.js";
+import { app, server } from "./socket/socket.js";
+
+configDotenv();
+
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true, // enable set cookie
+  })
+);
+
+app.use("/api/auth", authRouter);
+app.use("/api/messages", messagesRouter);
+app.use("/api/users", userRouter);
+
+app.use(globalErrorController);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  DB();
+  console.log(`Server running on port ${PORT}`.bgYellow);
+});
---
+++ FrontEnd/src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import AuthContextProvider from "./context/AuthContext.jsx";
+import SocketContextProvider from "./context/SocketContext.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <AuthContextProvider>
+    <SocketContextProvider>
+      <App />
+    </SocketContextProvider>
+  </AuthContextProvider>
+);
